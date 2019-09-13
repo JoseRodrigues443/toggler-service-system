fbc1f6a389c3e556b03cec331cf9a9380acbe6fe
José Miguel Rodrigues <JoseRodrigues@criticalmanufacturing.com>
2019-09-13 21:26:05 +0100

    UI basic components

---
+++ ui/toggler-manager/src/components/Navbar/NavBar.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'; // let's also import Component
+
+
+/**
+ * Toggle manager
+ */
+export class Navbar extends Component> {
+
+    /**
+     * Renders toggle manager
+     * @returns  
+     */
+    render() {
+        return <p>The current time is </p>
+    }
+}
+
+export default Navbar;
---
+++ ui/toggler-manager/src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const Home: React.FC = (props) => {
+  return (
+    <div className="Home">
+
+    </div>
+  );
+}
+
+export default Home;
